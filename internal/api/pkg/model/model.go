227bda20ce1bddbebbeefcfc995eec9cf1564019
Takacs Attila <takattila@gmail.com>
2023-06-23 14:25:35 +0200

    Added: api/model tests

--- internal/api/pkg/model/model.go
+++ internal/api/pkg/model/model.go
@@ -14,10 +14,12 @@ var (
 	Cfg *settings.Settings
 )
 
+// The Model structure contains the necessary data about the model name.
 type Model struct {
 	ModelName string `json:"model_name"`
 }
 
+// GetJSON provides a model name JSON.
 func GetJSON() string {
 	m := Model{}
 	m.getModelName()
@@ -27,6 +29,8 @@ func GetJSON() string {
 	return string(b)
 }
 
+// getModelName decides, whether the model name can be fetched from the configuration,
+// or it should be fetched from the OS, with: getModelNameOS function.
 func (m *Model) getModelName() *Model {
 	ret, _ := Cfg.GetStringSlice("on_runtime.commands.model_name")
 	if len(ret) == 0 {
@@ -36,6 +40,7 @@ func (m *Model) getModelName() *Model {
 	return m
 }
 
+// getModelNameOS fetces the model information: Kernel + Core + Architecture.
 func (m *Model) getModelNameOS() *Model {
 	info, _ := goInfo.GetInfo()
 	m.ModelName = strings.Title(info.Kernel + " " + info.Core + " " + runtime.GOARCH)
--- internal/api/pkg/model/model_test.go
+++ internal/api/pkg/model/model_test.go
@@ -1,1 +1,50 @@
 package model
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/takattila/monitor/pkg/common"
+	"github.com/takattila/settings-manager"
+)
+
+type (
+	ApiModelSuite struct {
+		suite.Suite
+	}
+)
+
+func (a ApiModelSuite) TestGetJSON() {
+	s := getConfig("api", "linux")
+	Cfg = s
+
+	JSON := GetJSON()
+	a.Contains(JSON, "model_name")
+
+	d := make(map[string]interface{})
+	err := json.Unmarshal([]byte(JSON), &d)
+	a.Equal(err, nil)
+}
+
+func (a ApiModelSuite) TestGetModelNameFromOS() {
+	s := getConfig("api", "linux")
+	s.Data.Set("on_runtime.commands.model_name", "")
+	Cfg = s
+
+	m := Model{}
+	m.getModelName()
+}
+
+func getConfig(service, system string) *settings.Settings {
+	gitRootPath := strings.ReplaceAll(common.Cli([]string{"bash", "-c", "git rev-parse --show-toplevel"}), "\n", "")
+	configPath := gitRootPath + "/configs/" + service + "." + system + ".yaml"
+	s := settings.New(configPath)
+	s.AutoReload()
+	return s
+}
+
+func TestApiModelSuite(t *testing.T) {
+	suite.Run(t, new(ApiModelSuite))
+}
