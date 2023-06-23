1a9c8c5f18eacda257254e31b66a05122d6baf55
Takacs Attila <takattila@gmail.com>
2023-06-23 09:27:15 +0200

    Added: config tests

--- internal/common/pkg/config/config.go
+++ internal/common/pkg/config/config.go
@@ -5,24 +5,28 @@ import (
 	"github.com/takattila/settings-manager"
 )
 
+// GetBool returns the value associated with the key as a boolean.
 func GetBool(s *settings.Settings, key string) bool {
 	ret, err := s.GetBool(key)
 	common.Error(err)
 	return ret
 }
 
+// GetInt returns the value associated with the key as an integer.
 func GetInt(s *settings.Settings, key string) int {
 	ret, err := s.GetInt(key)
 	common.Error(err)
 	return ret
 }
 
+// GetString returns the value associated with the key as a string.
 func GetString(s *settings.Settings, key string) string {
 	ret, err := s.GetString(key)
 	common.Error(err)
 	return ret
 }
 
+// GetStringSlice returns the value associated with the key as a slice of strings.
 func GetStringSlice(s *settings.Settings, key string) []string {
 	ret, err := s.GetStringSlice(key)
 	common.Error(err)
--- internal/common/pkg/config/config_test.go
+++ internal/common/pkg/config/config_test.go
@@ -1,1 +1,86 @@
 package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/takattila/settings-manager"
+)
+
+type (
+	ConfigSuite struct {
+		suite.Suite
+	}
+)
+
+func (s ConfigSuite) TestTGetBool() {
+	content := `
+other:
+  content:
+    int: 1
+    string: 'text'
+    bool: true
+    slice:
+      - a
+      - b
+      - c
+`
+	sm := settings.NewFromContent(content)
+	result := GetBool(sm, "other.content.bool")
+	s.Equal(true, result)
+}
+
+func (s ConfigSuite) TestGetInt() {
+	content := `
+other:
+  content:
+    int: 1
+    string: 'text'
+    bool: true
+    slice:
+      - a
+      - b
+      - c
+`
+	sm := settings.NewFromContent(content)
+	result := GetInt(sm, "other.content.int")
+	s.Equal(1, result)
+}
+
+func (s ConfigSuite) TestGetString() {
+	content := `
+other:
+  content:
+    int: 1
+    string: 'text'
+    bool: true
+    slice:
+      - a
+      - b
+      - c
+`
+	sm := settings.NewFromContent(content)
+	result := GetString(sm, "other.content.string")
+	s.Equal("text", result)
+}
+
+func (s ConfigSuite) TestGetStringSlice() {
+	content := `
+other:
+  content:
+    int: 1
+    string: 'text'
+    bool: true
+    slice:
+      - a
+      - b
+      - c
+`
+	sm := settings.NewFromContent(content)
+	result := GetStringSlice(sm, "other.content.slice")
+	s.Equal([]string{"a", "b", "c"}, result)
+}
+
+func TestConfigSuite(t *testing.T) {
+	suite.Run(t, new(ConfigSuite))
+}
