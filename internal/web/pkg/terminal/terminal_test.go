26f46a6398407c18a2f44cd3ead9ee5a5403f7b8
Takacs Attila <takattila@gmail.com>
2023-06-27 13:03:19 +0200

    Added: web/terminal tests

--- internal/web/pkg/terminal/terminal_test.go
+++ internal/web/pkg/terminal/terminal_test.go
@@ -1,1 +1,26 @@
 package terminal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// Testing:
+// go test -coverprofile="coverage.out" -v ./...
+// go tool cover -html="coverage.out"
+
+type (
+	WebSessionSuite struct {
+		suite.Suite
+	}
+)
+
+func (s WebSessionSuite) TestPrompt() {
+	input := Prompt("username: ")
+	s.Equal("", input)
+}
+
+func TestWebSessionSuite(t *testing.T) {
+	suite.Run(t, new(WebSessionSuite))
+}
