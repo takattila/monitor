2478241b491f90c347d8d41b89e6e2f2ae577e61
Takacs Attila <takattila@gmail.com>
2023-06-29 14:12:28 +0200

    Added future features to playground

--- internal/api/pkg/playground/playground.go
+++ internal/api/pkg/playground/playground.go
@@ -2,26 +2,75 @@ package playground
 
 import (
 	"fmt"
-	"runtime"
 
-	"github.com/matishsiao/goInfo"
+	"github.com/shirou/gopsutil/host"
+	"github.com/takattila/monitor/pkg/logger"
 )
 
 // Playground for testing stuff.
 func Playground() string {
+	l := logger.New(logger.DebugLevel, logger.ColorOn)
 	html := "<html><head><title>Go Playground</title></head><body><h1>Go</h1>"
 
-	info, _ := goInfo.GetInfo()
+	up := GetUptime()
+	l.Error(up.Error)
+	l.Info(fmt.Sprintf("%d days, %d hours, %d minutes", up.Days, up.Hours, up.Minutes))
+	html = html + fmt.Sprintf("%d days, %d hours, %d minutes", up.Days, up.Hours, up.Minutes) + "<br>"
 
-	html = html + "GoOS:" + info.GoOS + "<br>"
-	html = html + "Kernel:" + info.Kernel + "<br>"
-	html = html + "Core:" + info.Core + "<br>"
-	html = html + "Platform:" + info.Platform + "<br>"
-	html = html + "OS:" + info.OS + "<br>"
-	html = html + "Hostname:" + info.Hostname + "<br>"
-	html = html + "CPUs:" + fmt.Sprint(info.CPUs) + "<br>"
-	html = html + "Arch:" + runtime.GOARCH + "<br>"
+	p := GetPlatform()
+	l.Error(p.Error)
+	l.Info(fmt.Sprintf("platform: %s, family: %s, version: %s", p.Name, p.Family, p.Version))
+	html = html + fmt.Sprintf("platform: %s, family: %s, version: %s", p.Name, p.Family, p.Version) + "<br>"
+
+	t, err := GetTemp()
+	l.Error(err)
+	l.Info(fmt.Sprintf("temps: %s", t))
+	html = html + fmt.Sprintf("temps: %s", t) + "<br>"
 
 	html = html + "</body></html>"
 	return html
+}
+
+type UpTime struct {
+	Days    uint64
+	Hours   uint64
+	Minutes uint64
+	Error   error
+}
+
+func GetUptime() (up UpTime) {
+	uptime, err := host.Uptime()
+	up.Error = err
+
+	days := uptime / (60 * 60 * 24)
+	hours := (uptime - (days * 60 * 60 * 24)) / (60 * 60)
+	minutes := ((uptime - (days * 60 * 60 * 24)) - (hours * 60 * 60)) / 60
+
+	up.Days = days
+	up.Hours = hours
+	up.Minutes = minutes
+
+	return up
+}
+
+type Platform struct {
+	Name    string
+	Family  string
+	Version string
+	Error   error
+}
+
+func GetPlatform() (p Platform) {
+	platform, family, version, err := host.PlatformInformation()
+	p.Error = err
+
+	p.Name = platform
+	p.Family = family
+	p.Version = version
+
+	return p
+}
+
+func GetTemp() ([]host.TemperatureStat, error) {
+	return host.SensorsTemperatures()
 }
