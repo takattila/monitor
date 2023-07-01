846980985688781baec8b56b1464a58ddf3b851e
Takacs Attila <takattila@gmail.com>
2023-07-01 14:21:18 +0200

    Added: checkPythonsIsInstalled in: get-latest-binaries.sh

--- scripts/get-latest-binaries.sh
+++ scripts/get-latest-binaries.sh
@@ -103,6 +103,26 @@ function checkProgramIsInstalled {
     echo $?
 }
 
+function checkPythonsIsInstalled {
+    local program="$1"
+    local python
+
+    if [[ "$program" = "python" ]]; then
+        python=$(ls /usr/bin/* | grep python | head -n 1)
+        if [[ "$python" != "" ]]; then
+            echo -e "    - There is another version of python installed: '${YELLOW}${python}${ENDCOLOR}'."
+            read -r -p $'    - '$(echo -e "${YELLOW}")'Do you want to use this python version?'$(echo -e "${ENDCOLOR}")' [y/N] ' backupCfg
+            if [[ "$backupCfg" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+                echo -e "      - Creating symlink: '${YELLOW}ln -s $(which "${python}") /usr/bin/python${ENDCOLOR}'..."
+                sudo ln -s $(which "${python}") /usr/bin/python
+                main
+            else
+                echo -e "      - The symlink cration of: '${YELLOW}${python}${ENDCOLOR}' skipped..."
+            fi
+        fi
+    fi
+}
+
 function checkAllProgramsInstalled {
     local shouldBeInstalled
     local check
@@ -128,7 +148,8 @@ function checkAllProgramsInstalled {
         echo -e "${ERROR} For a successful installation, the following programs must be installed:"
         for program in ${!shouldBeInstalled[@]}; do
             if [[ "${shouldBeInstalled[$program]}" = "1" ]]; then
-                echo "- $program"
+                echo "  - $program"
+                checkPythonsIsInstalled "$program"
             fi
         done
         exit 1
