e97b9c40862f1f408740ccd45b07446aafb7de9c
Takacs Attila <takattila@gmail.com>
2025-08-06 21:17:31 +0200

    Change scripts\coverage.sh file endings to LF

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-# Csak nem-main csomagok listázása
+# List packages excluding the main package
 filtered_packages=$(go list -f '{{.Name}} {{.ImportPath}}' ./... | awk '$1 != "main" {print $2}')
 
-# Tesztek futtatása párhuzamosan
+# Run tests in parallel with coverage, saving report to coverage.out
 go test -coverprofile="coverage.out" -covermode=atomic -v -p 4 $filtered_packages
 
-# Lefedettségi jelentés
+# Show coverage report summary
 go tool cover -func=coverage.out
-
