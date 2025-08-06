d8f3b259b65f986ae9c798fe44b5a3dcd0600cb6
Takacs Attila <takattila@gmail.com>
2025-08-06 20:11:29 +0200

    Do not test main packages in scripts/coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-go test -coverprofile="coverage.out" -covermode="atomic" -v ./...
-go tool cover -func coverage.out 
+# List all packages
+all_packages=$(go list ./...)
+
+# Filter: only include packages that do NOT contain 'package main'
+filtered_packages=()
+for pkg in $all_packages; do
+  # Get the directory of the package
+  pkg_dir=$(go list -f '{{.Dir}}' "$pkg")
+
+  # If no 'package main' is found in the files, include the package
+  if ! grep -q '^package main' "$pkg_dir"/*.go 2>/dev/null; then
+    filtered_packages+=("$pkg")
+  fi
+done
+
+# Run tests on the filtered packages
+go test -coverprofile="coverage.out" -covermode=atomic -v "${filtered_packages[@]}"
+
+# Generate coverage report
+go tool cover -func=coverage.out
+
